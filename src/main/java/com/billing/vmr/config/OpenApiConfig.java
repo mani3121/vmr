918ca97f5e2015a81fc7447572294fdac18b61bb
mani3121 <maniavudai10@gmail.com>
2024-12-02 21:51:33 +0530

    Update OpenApiConfig.java

--- src/main/java/com/billing/vmr/config/OpenApiConfig.java
+++ src/main/java/com/billing/vmr/config/OpenApiConfig.java
@@ -1,17 +1,1 @@
-package com.billing.vmr.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-@Configuration
-public class OpenApiConfig {
-    @Bean
-    public OpenAPI mandatoryparamGlobalParamter() {
-        return new OpenAPI()
-                .components(new Components());
-    }
-}
